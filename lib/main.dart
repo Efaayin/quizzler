11e8292767496b753efa6b7a7435106b62160c06
Ifeanyi <alichiifeanyi@yahoo.com>
2021-06-30 11:13:07 +0100

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class _QuizPageState extends State<QuizPage> {
 
   void nextNumber() {
     setState(() {
-      if (questionNumber < 2) {
+      if (questionNumber < (quizBrain.questionBank.length - 1)) {
         questionNumber++;
       }
     });
@@ -153,9 +153,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
